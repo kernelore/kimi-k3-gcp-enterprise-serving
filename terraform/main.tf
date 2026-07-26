70829f3e19fba7e75061d3d7e468ec274635d951
kernelore <kernelore@users.noreply.github.com>
2026-07-26 09:45:26 +0000

    chore(terraform): bump google and google-beta providers to version 7.41 and verify compatibility

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,11 +8,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = "~> 7.41"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 6.0"
+      version = "~> 7.41"
     }
   }
 }
